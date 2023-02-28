7da026ecc55418452444a628cd8160683e5e5059
d86leader <nikita@dfns.co>
2023-02-28 17:23:42 +0100

    Add measuring refresh perf; make sign bench conditional

--- tests/src/bin/measure_perf.rs
+++ tests/src/bin/measure_perf.rs
@@ -1,7 +1,7 @@
 use anyhow::Context;
 use cggmp21::{
     progress::PerfProfiler,
-    signing::{Message, Msg},
+    signing::Message,
     ExecutionId,
 };
 use cggmp21_tests::PrecomputedKeyShares;
@@ -25,6 +25,10 @@ lazy_static::lazy_static! {
 struct Args {
     #[clap(short, default_value = "3,5,7,10")]
     n: Vec<u16>,
+    #[clap(long)]
+    bench_refresh: bool,
+    #[clap(long)]
+    bench_signing: bool,
 }
 
 #[ignore = "performance tests are ignored by default"]
@@ -39,37 +43,72 @@ async fn main() {
             .get_shares::<E>(n)
             .expect("retrieve key shares from cache");
 
-        let signing_execution_id: [u8; 32] = rng.gen();
-        let signing_execution_id = ExecutionId::<E, L>::from_bytes(&signing_execution_id);
+        if args.bench_refresh {
+            let refresh_execution_id: [u8; 32] = rng.gen();
+            let refresh_execution_id =
+                ExecutionId::<E, L>::from_bytes(&refresh_execution_id);
 
-        let message_to_sign = b"Dfns rules!";
-        let message_to_sign = Message::new::<Sha256>(message_to_sign);
+            use cggmp21::key_refresh::Msg;
+            let mut simulation = Simulation::<Msg<E, D>>::new();
 
-        let mut simulation = Simulation::<Msg<E, D>>::new();
+            let outputs = shares.iter().map(|share| {
+                let party = simulation.add_party();
+                let refresh_execution_id = refresh_execution_id.clone();
+                let mut party_rng = ChaCha20Rng::from_seed(rng.gen());
 
-        let mut outputs = vec![];
-        for share in &shares {
-            let party = simulation.add_party();
-            let signing_execution_id = signing_execution_id.clone();
-            let mut party_rng = ChaCha20Rng::from_seed(rng.gen());
+                let mut profiler = PerfProfiler::new();
 
-            let mut profiler = PerfProfiler::new();
+                async move {
+                    let _new_share = cggmp21::key_refresh(share)
+                        .set_execution_id(refresh_execution_id)
+                        .set_progress_tracer(&mut profiler)
+                        .start(&mut party_rng, party)
+                        .await
+                        .context("refresh failed")?;
+                    profiler.get_report().context("get perf report")
+                }
+            });
 
-            outputs.push(async move {
-                let _signature = cggmp21::signing(share)
-                    .set_execution_id(signing_execution_id)
-                    .set_progress_tracer(&mut profiler)
-                    .sign(&mut party_rng, party, message_to_sign)
-                    .await
-                    .context("signing failed")?;
-                profiler.get_report().context("get perf report")
-            })
+            let perf_reports = futures::future::try_join_all(outputs)
+                .await
+                .expect("signing failed");
+            println!("{}", perf_reports[0].clone().display_io(false));
         }
 
-        let perf_reports = futures::future::try_join_all(outputs)
-            .await
-            .expect("signing failed");
+        if args.bench_signing {
+            let signing_execution_id: [u8; 32] = rng.gen();
+            let signing_execution_id = ExecutionId::<E, L>::from_bytes(&signing_execution_id);
 
-        println!("{}", perf_reports[0].clone().display_io(false));
+            let message_to_sign = b"Dfns rules!";
+            let message_to_sign = Message::new::<Sha256>(message_to_sign);
+
+            use cggmp21::signing::Msg;
+            let mut simulation = Simulation::<Msg<E, D>>::new();
+
+            let mut outputs = vec![];
+            for share in &shares {
+                let party = simulation.add_party();
+                let signing_execution_id = signing_execution_id.clone();
+                let mut party_rng = ChaCha20Rng::from_seed(rng.gen());
+
+                let mut profiler = PerfProfiler::new();
+
+                outputs.push(async move {
+                    let _signature = cggmp21::signing(share)
+                        .set_execution_id(signing_execution_id)
+                        .set_progress_tracer(&mut profiler)
+                        .sign(&mut party_rng, party, message_to_sign)
+                        .await
+                        .context("signing failed")?;
+                    profiler.get_report().context("get perf report")
+                })
+            }
+
+            let perf_reports = futures::future::try_join_all(outputs)
+                .await
+                .expect("signing failed");
+
+            println!("{}", perf_reports[0].clone().display_io(false));
+        }
     }
 }
