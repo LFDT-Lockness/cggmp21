80fc4c756e65a9ee364422102369dd4cf88f0175
Denis Varlakov <cordial.egg6523@fastmail.com>
2023-03-09 13:38:58 +0000

    Remove unused deps

--- cggmp21/src/lib.rs
+++ cggmp21/src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(non_snake_case, mixed_script_confusables, uncommon_codepoints)]
-#![forbid(clippy::disallowed_methods)]
+#![forbid(clippy::disallowed_methods, unused_crate_dependencies)]
 
 pub use {
     paillier_zk, paillier_zk::libpaillier, paillier_zk::libpaillier::unknown_order, round_based,
