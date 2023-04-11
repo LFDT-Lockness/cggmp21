c54a8ee259acdd5f1235a7de4e7a14d368ca9d10
Denis Varlakov <cordial.egg6523@fastmail.com>
2023-04-11 10:27:42 +0000

    Fix typo

--- cggmp21/src/signing.rs
+++ cggmp21/src/signing.rs
@@ -204,7 +204,7 @@ where
     ///
     /// CGGMP21 signing protocol requires message in the first round to be sent over reliable
     /// broadcast channel. We ensure reliability of the broadcast channel by introducing extra
-    /// communication round (at const of additional latency). You may disable it, for instance,
+    /// communication round (at cost of additional latency). You may disable it, for instance,
     /// if your transport layer is reliable by construction (e.g. you use blockchain for
     /// communications).
     ///
