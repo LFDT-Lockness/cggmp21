cb699438d6f2e7d576d3db70edc9a4a9b0912950
d86leader <nikita@dfns.co>
2024-02-16 14:34:55 +0100

    Prepare to publish secondary crates

--- shell.nix
+++ shell.nix
@@ -1,23 +1,26 @@
 let # Rust
-    rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
-    pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-    rustVersion = "1.75.0";
-    rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-      extensions = [
-        "rust-src" # for rust-analyzer
-      ];
-    };
-    # Latex
-    tex = (pkgs.texlive.combine {
-      inherit (pkgs.texlive) scheme-small
-        collection-mathscience preprint;
-    });
-    
+  pkgs = import <nixpkgs> { overlays = [ rustOverlay ]; };
+  lib = pkgs.lib;
+  isDarwin = pkgs.hostPlatform.isDarwin;
+
+  rustVersion = "1.75.0";
+  rustOverlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+
+  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+    ];
+  };
+  # Latex
+  tex = (pkgs.texlive.combine {
+    inherit (pkgs.texlive) scheme-small
+      collection-mathscience preprint;
+  });
 
 in pkgs.stdenv.mkDerivation {
   name = "signers-env";
   nativeBuildInputs = [
     rust pkgs.rust-analyzer tex
   ];
-  buildInputs = [];
+  buildInputs = lib.optionals isDarwin [pkgs.darwin.apple_sdk.frameworks.Security];
 }
