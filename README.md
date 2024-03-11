75ef9d86da26d754be68ec2342c7209868ec007b
Denis Varlakov <denis@dfns.co>
2024-03-11 09:33:59 +0100

    Update docs

--- README.md
+++ README.md
@@ -9,28 +9,35 @@ identifiable abort, provides two signing protocols (3+1 and 5+1 rounds with diff
 of abort identification) and key refresh protocol out of the box.
 
 This crate implements:
-* General threshold (i.e., t-out-of-n) and full threshold (i.e., n-out-of-n) key generation
-* (3+1)-round general threshold and full threshold signing
+* Threshold (i.e., t-out-of-n) and non-threshold (i.e., n-out-of-n) key generation
+* (3+1)-round general threshold and non-threshold signing
 * Auxiliary info generation protocol
-* Key refresh for full-threshold keys
+* Key refresh for non-threshold keys
 * HD-wallets support based on [slip10] standard (compatible with [bip32]) \
   Requires `hd-wallets` feature
+
+A self-contained description of the protocols we implemented is available [here][the spec].
 
 We also provide auxiliary tools like:
 * Secret key reconstruction (exporting key from TSS)
 * Trusted dealer (importing key into TSS)
 
 This crate **does not** (currently) support:
-* Key refresh for general thresholds
+* Key refresh for threshold keys (i.e., t-out-of-n)
 * Identifiable abort
 * The (5+1)-round signing protocol
+
+> About notion of threshold and non-threshold keys: originally, CGGMP21 paper does not have support of
+arbitrary `t` and only works with non-threshold n-out-of-n keys. We have added support of arbitrary
+threshold $2 \le t \le n$, however, we made it possible to opt out therhsoldness so original CGGMP21
+protocol can be carried out if needed.
 
 ## Running the protocol
 
 ### Networking
-In order to run the protocol, you need to define how each signer can communicate with other signers. We
-use a `round_based` framework that handles networking. Basically, you need to define a stream
-of `incoming` messages and sink of `outgoing` messages:
+The most essential part of running an interactive protocol is to define how parties can communicate with
+each other. Our `cggmp21` library is agnostic to the network layer and only requires you to provide two
+things: a stream of incoming messages and a sink for outgoing messages, i.e.:
 
 ```rust
 let incoming: impl Stream<Item = Result<Incoming<Msg>>>;
@@ -42,59 +49,52 @@ where:
 * `round_based::Incoming` and `round_based::Outgoing` wrap `Msg` and provide additional data (e.g., sender/recepient)
 * `futures::Stream` and `futures::Sink` are well-known async primitives.
 
-Then, construct a `round_based::MpcParty`:
+Once you have that, you can construct an [MpcParty][round_based::MpcParty]:
 ```rust
 let delivery = (incoming, outgoing);
 let party = round_based::MpcParty::connected(delivery);
 ```
 
-#### Signer indexes
-Each signer in a protocol execution (keygen/signing/etc.) occupies a unique index $i$ ($0 \le i < n$,
-where $n$ is number of parties overall). For instance, if Signer A occupies index `2`, then all
-other signers must agree that `i=2` corresponds to Signer A.
+The concrete networking implementation to use will depend heavily on the specific application.
+Some applications may use libp2p; others may prefer having a central delivery server or a database
+(like Redis or Postgres); some specific applications may want to communicate over a public
+blockchain, and so on.
 
-Assuming you have a PKI (which is anyway needed to comply with [security requirements]) and each signer
-has a public key uniqely idenitifying that signer, you can assign unique indexes to the signers as follows:
-1. Make a list of signers' public keys
-2. Sort the list of public keys
-3. Assign each signer an index `i` such that `i` corresponds to the position of the signer's public key in the
-   sorted list of public keys
+Whatever networking implementation you use, keep in mind that:
 
-[security requirements]: #security
+* All messages must be authenticated \
+  Whenever one party receives a message from another, the receiver should cryptographically
+  verify that the message comes from the claimed sender.
+* All p2p messages must be encrypted \
+  Only the designated recipient should be able to read the message
 
-#### Security
-Make sure that the communication layer complies with security requirements:
-* All messages sent must be authenticated
-* All p2p messages must be encrypted
+#### Signer indices
+Our library uses indices to uniquely refer to particular signers sharing a key. Each index `i`
+is an unsigned integer `u16` with $0 \le i < n$ where `n` is the total number of parties.
+
+All signers should have the same view about each others' indices. For instance, if Signer A
+holds index 2, then all other signers must agree that i=2 corresponds to Signer A.
+
+Assuming some sort of PKI (which would anyway likely be used to ensure secure communication,
+as described above), each signer has a public key that uniquely identifies that signer.
+It is then possible to assign unique indices to the signers by lexicographically sorting the
+signers' public keys, and letting the index of a signer be the position of that signer's public
+key in the sorted list.
 
 ### Execution ID
-When executing a protocol, signers need to agree on a unique identifier of the protocol execution `ExecutionId`.
-The Execution ID needs to be unique per protocol execution (keygen/signing/etc.), otherwise it may compromise security.
-The Execution ID needs to be the same for all signers taking part in the protocol, otherwise protocol will abort.
-Execution ID **does not** need to be secret.
+Execution of our protocols requires all participants to agree on unique execution ID (aka
+session identifier) that is assumed never to repeat. This string provides context separation
+between different executions of the protocol to ensure that an adversary cannot replay messages
+from one execution to another.
 
-Once signers can talk to each other and share an execution ID, they're ready to generate a key!
-
-### Distributed Key Generation
-```rust
-use cggmp21::supported_curves::Secp256k1;
-
-let eid = cggmp21::ExecutionId::new(b"execution id, unique per protocol execution");
-let i = /* signer index (0 <= i < n) */;
-let n = /* number of signers taking part in key generation */;
-let t = /* threshold */;
-
-let incomplete_key_share = cggmp21::keygen::<Secp256k1>(eid, i, n)
-    .set_threshold(t)
-    .start(&mut OsRng, party)
-    .await?;
-```
-This code outputs `IncompleteKeyShare`. Note that this key share is not yet ready to do signing. You need to “complete” it
-by generating auxiliary info (see below).
+Once signers can talk to each other and share an execution ID, they're ready to do MPC!
 
 ### Auxiliary info generation
-After key generation, all signers need to take part in generation of auxiliary information. Make sure all signers occupy exactly
-the same indexes as at keygen.
+In the usual flow, signers run a protocol for auxiliary-data generation before running distributed
+key generation. This protocol sets up certain parameters (in particular, Paillier moduli
+for each of the signers) that will be used during the signing protocols. This protocol can be
+run as follows:
+
 ```rust
 // Prime generation can take a while
 let pregenerated_primes = cggmp21::PregeneratedPrimes::generate(&mut OsRng);
@@ -108,15 +108,48 @@ let aux_info = cggmp21::aux_info_gen(eid, i, n, pregenerated_primes)
     .await?;
 ```
 
-After keygen and aux info gen are done, you can make a “complete” key share that can be used for signing:
+The auxiliary-data generation protocol is computationally heavy as it requires the generation
+of safe primes and involves several zero-knowledge (ZK) proofs.
+
+#### On reusability of the auxiliary data
+The CGGMP21 paper assumes that new auxiliary data is generated for each secret key that is shared.
+However, examination of the proof shows that this is not necessary, and a fixed group of signers
+can use the same auxiliary data for the secure sharing/usage of multiple keys.
+
+### Distributed Key Generation (DKG)
+The DKG protocol involves all signers who will co-share a key. All signers need to agree on
+some basic parameters including the participants' indices, the execution ID, and the
+threshold value (i.e., t). The protocol can be executed as
+
+```rust
+use cggmp21::supported_curves::Secp256k1;
+
+let eid = cggmp21::ExecutionId::new(b"execution id, unique per protocol execution");
+let i = /* signer index (0 <= i < n) */;
+let n = /* number of signers taking part in key generation */;
+let t = /* threshold */;
+
+let incomplete_key_share = cggmp21::keygen::<Secp256k1>(eid, i, n)
+    .set_threshold(t)
+    .start(&mut OsRng, party)
+    .await?;
+```
+
+The above produces an `IncompleteKeyShare`. An incomplete key share can be saved on disk by serializing using
+[`serde` crate][serde]. Treat this material appropriately as it contains sensitive information.
+
+Assuming auxiliary-data generation has already been done (see above), you can "complete" the
+key share using:
+
 ```rust
 let key_share = cggmp21::KeyShare::from_parts((incomplete_key_share, aux_info))?;
 ```
 
 ### Signing
-Once a complete key share is obtained, signers can sign or generate presignatures. In either case, the required threshold t of
-signers must take part in the protocol. Each signer needs to be assigned a unique index
-`0 <= i < t`, but we also need to know which index each signer had at keygen.
+Once signers have a set of "completed" key shares, they can sign or generate presignatures.
+In either case, exactly the threshold number (i.e., t) of signers must take part in the protocol.
+As in the DKG protocol, each signer needs to be assigned a unique index, now in the range from 0
+to t-1. But the signers also need to know which index each signer occupied at the time of keygen.
 
 In the example below, we do a full signing:
 ```rust
@@ -138,11 +171,11 @@ Alternatively, you can generate a presignature and later use it to sign:
 1. Use `SigningBuilder::generate_presignature` to run the presignature generation protocol
 2. Later, when a signing request is received, each signer issues a partial signature using
    `Presignature::issue_partial_signature`
-3. The requisite number of partial signatures can be combined using `PartialSignature::combine` to
+3. A threshold number of partial signatures can be combined using `PartialSignature::combine` to
    obtain a full signature
 
 **Never reuse presignatures!** If you use the same presignature to sign two different messages,
-it leaks information about key shares to anyone who can observe the signatures.
+the private key may be leaked.
 
 ## HD wallets support
 Library supports non-hardened deterministic key derivation based on [slip10] standard (compatible
--- cggmp21/src/lib.rs
+++ cggmp21/src/lib.rs
@@ -9,28 +9,35 @@
 //! of abort identification) and key refresh protocol out of the box.
 //!
 //! This crate implements:
-//! * General threshold (i.e., t-out-of-n) and full threshold (i.e., n-out-of-n) key generation
-//! * (3+1)-round general threshold and full threshold signing
+//! * Threshold (i.e., t-out-of-n) and non-threshold (i.e., n-out-of-n) key generation
+//! * (3+1)-round general threshold and non-threshold signing
 //! * Auxiliary info generation protocol
-//! * Key refresh for full-threshold keys
+//! * Key refresh for non-threshold keys
 //! * HD-wallets support based on [slip10] standard (compatible with [bip32]) \
 //!   Requires `hd-wallets` feature
+//!
+//! A self-contained description of the protocols we implemented is available [here][the spec].
 //!
 //! We also provide auxiliary tools like:
 //! * [Secret key reconstruction](crate::key_share::reconstruct_secret_key) (exporting key from TSS)
 //! * [Trusted dealer](crate::trusted_dealer) (importing key into TSS)
 //!
 //! This crate **does not** (currently) support:
-//! * Key refresh for general thresholds
+//! * Key refresh for threshold keys (i.e., t-out-of-n)
 //! * Identifiable abort
 //! * The (5+1)-round signing protocol
+//!
+//! > About notion of threshold and non-threshold keys: originally, CGGMP21 paper does not have support of
+//! arbitrary `t` and only works with non-threshold n-out-of-n keys. We have added support of arbitrary
+//! threshold $2 \le t \le n$, however, we made it possible to opt out therhsoldness so original CGGMP21
+//! protocol can be carried out if needed.
 //!
 //! ## Running the protocol
 //!
 //! ### Networking
-//! In order to run the protocol, you need to define how each signer can communicate with other signers. We
-//! use a [`round_based`] framework that handles networking. Basically, you need to define a stream
-//! of `incoming` messages and sink of `outgoing` messages:
+//! The most essential part of running an interactive protocol is to define how parties can communicate with
+//! each other. Our `cggmp21` library is agnostic to the network layer and only requires you to provide two
+//! things: a stream of incoming messages and a sink for outgoing messages, i.e.:
 //!
 //! ```rust,ignore
 //! let incoming: impl Stream<Item = Result<Incoming<Msg>>>;
@@ -42,7 +49,7 @@
 //! * [`round_based::Incoming`] and [`round_based::Outgoing`] wrap `Msg` and provide additional data (e.g., sender/recepient)
 //! * [`futures::Stream`] and [`futures::Sink`] are well-known async primitives.
 //!
-//! Then, construct a [`round_based::MpcParty`]:
+//! Once you have that, you can construct an [MpcParty][round_based::MpcParty]:
 //! ```rust
 //! # type Msg = cggmp21::signing::msg::Msg<cggmp21::supported_curves::Secp256k1, sha2::Sha256>;
 //! # let incoming = futures::stream::pending::<Result<round_based::Incoming<Msg>, std::convert::Infallible>>();
@@ -51,34 +58,83 @@
 //! let party = round_based::MpcParty::connected(delivery);
 //! ```
 //!
-//! #### Signer indexes
-//! Each signer in a protocol execution (keygen/signing/etc.) occupies a unique index $i$ ($0 \le i < n$,
-//! where $n$ is number of parties overall). For instance, if Signer A occupies index `2`, then all
-//! other signers must agree that `i=2` corresponds to Signer A.
+//! The concrete networking implementation to use will depend heavily on the specific application.
+//! Some applications may use libp2p; others may prefer having a central delivery server or a database
+//! (like Redis or Postgres); some specific applications may want to communicate over a public
+//! blockchain, and so on.
 //!
-//! Assuming you have a PKI (which is anyway needed to comply with [security requirements]) and each signer
-//! has a public key uniqely idenitifying that signer, you can assign unique indexes to the signers as follows:
-//! 1. Make a list of signers' public keys
-//! 2. Sort the list of public keys
-//! 3. Assign each signer an index `i` such that `i` corresponds to the position of the signer's public key in the
-//!    sorted list of public keys
+//! Whatever networking implementation you use, keep in mind that:
 //!
-//! [security requirements]: #security
+//! * All messages must be authenticated \
+//!   Whenever one party receives a message from another, the receiver should cryptographically
+//!   verify that the message comes from the claimed sender.
+//! * All p2p messages must be encrypted \
+//!   Only the designated recipient should be able to read the message
 //!
-//! #### Security
-//! Make sure that the communication layer complies with security requirements:
-//! * All messages sent must be authenticated
-//! * All p2p messages must be encrypted
+//! #### Signer indices
+//! Our library uses indices to uniquely refer to particular signers sharing a key. Each index `i`
+//! is an unsigned integer `u16` with $0 \le i < n$ where `n` is the total number of parties.
+//!
+//! All signers should have the same view about each others' indices. For instance, if Signer A
+//! holds index 2, then all other signers must agree that i=2 corresponds to Signer A.
+//!
+//! Assuming some sort of PKI (which would anyway likely be used to ensure secure communication,
+//! as described above), each signer has a public key that uniquely identifies that signer.
+//! It is then possible to assign unique indices to the signers by lexicographically sorting the
+//! signers' public keys, and letting the index of a signer be the position of that signer's public
+//! key in the sorted list.
 //!
 //! ### Execution ID
-//! When executing a protocol, signers need to agree on a unique identifier of the protocol execution [`ExecutionId`].
-//! The Execution ID needs to be unique per protocol execution (keygen/signing/etc.), otherwise it may compromise security.
-//! The Execution ID needs to be the same for all signers taking part in the protocol, otherwise protocol will abort.
-//! Execution ID **does not** need to be secret.
+//! Execution of our protocols requires all participants to agree on unique execution ID (aka
+//! session identifier) that is assumed never to repeat. This string provides context separation
+//! between different executions of the protocol to ensure that an adversary cannot replay messages
+//! from one execution to another.
 //!
-//! Once signers can talk to each other and share an execution ID, they're ready to generate a key!
+//! Once signers can talk to each other and share an execution ID, they're ready to do MPC!
 //!
-//! ### Distributed Key Generation
+//! ### Auxiliary info generation
+//! In the usual flow, signers run a protocol for auxiliary-data generation before running distributed
+//! key generation. This protocol sets up certain parameters (in particular, Paillier moduli
+//! for each of the signers) that will be used during the signing protocols. This protocol can be
+//! run as follows:
+//!
+//! ```rust,no_run
+//! # async fn doc() -> Result<(), cggmp21::KeyRefreshError> {
+//! # type Msg = cggmp21::key_refresh::msg::aux_only::Msg<sha2::Sha256, cggmp21::security_level::SecurityLevel128>;
+//! # let incoming = futures::stream::pending::<Result<round_based::Incoming<Msg>, std::convert::Infallible>>();
+//! # let outgoing = futures::sink::drain::<round_based::Outgoing<Msg>>();
+//! # let delivery = (incoming, outgoing);
+//! # let party = round_based::MpcParty::connected(delivery);
+//! #
+//! # use rand_core::OsRng;
+//! // Prime generation can take a while
+//! let pregenerated_primes = cggmp21::PregeneratedPrimes::generate(&mut OsRng);
+//!
+//! let eid = cggmp21::ExecutionId::new(b"execution id, unique per protocol execution");
+//! let i = /* signer index, same as at keygen */
+//! # 0;
+//! let n = /* number of signers */
+//! # 3;
+//!
+//! let aux_info = cggmp21::aux_info_gen(eid, i, n, pregenerated_primes)
+//!     .start(&mut OsRng, party)
+//!     .await?;
+//! # Ok(()) }
+//! ```
+//!
+//! The auxiliary-data generation protocol is computationally heavy as it requires the generation
+//! of safe primes and involves several zero-knowledge (ZK) proofs.
+//!
+//! #### On reusability of the auxiliary data
+//! The CGGMP21 paper assumes that new auxiliary data is generated for each secret key that is shared.
+//! However, examination of the proof shows that this is not necessary, and a fixed group of signers
+//! can use the same auxiliary data for the secure sharing/usage of multiple keys.
+//!
+//! ### Distributed Key Generation (DKG)
+//! The DKG protocol involves all signers who will co-share a key. All signers need to agree on
+//! some basic parameters including the participants' indices, the execution ID, and the
+//! threshold value (i.e., t). The protocol can be executed as
+//!
 //! ```rust,no_run
 //! # async fn doc() -> Result<(), cggmp21::KeygenError> {
 //! # type Msg = cggmp21::keygen::msg::threshold::Msg<cggmp21::supported_curves::Secp256k1, cggmp21::security_level::SecurityLevel128, sha2::Sha256>;
@@ -104,48 +160,24 @@
 //!     .await?;
 //! # Ok(()) }
 //! ```
-//! This code outputs [`IncompleteKeyShare`]. Note that this key share is not yet ready to do signing. You need to “complete” it
-//! by generating auxiliary info (see below).
 //!
-//! ### Auxiliary info generation
-//! After key generation, all signers need to take part in generation of auxiliary information. Make sure all signers occupy exactly
-//! the same indexes as at keygen.
+//! The above produces an [`IncompleteKeyShare`]. An incomplete key share can be saved on disk by serializing using
+//! [`serde` crate][serde]. Treat this material appropriately as it contains sensitive information.
+//!
+//! Assuming auxiliary-data generation has already been done (see above), you can "complete" the
+//! key share using:
+//!
 //! ```rust,no_run
-//! # async fn doc() -> Result<(), cggmp21::KeyRefreshError> {
-//! # type Msg = cggmp21::key_refresh::msg::aux_only::Msg<sha2::Sha256, cggmp21::security_level::SecurityLevel128>;
-//! # let incoming = futures::stream::pending::<Result<round_based::Incoming<Msg>, std::convert::Infallible>>();
-//! # let outgoing = futures::sink::drain::<round_based::Outgoing<Msg>>();
-//! # let delivery = (incoming, outgoing);
-//! # let party = round_based::MpcParty::connected(delivery);
-//! #
-//! # use rand_core::OsRng;
-//! // Prime generation can take a while
-//! let pregenerated_primes = cggmp21::PregeneratedPrimes::generate(&mut OsRng);
-//!
-//! let eid = cggmp21::ExecutionId::new(b"execution id, unique per protocol execution");
-//! let i = /* signer index, same as at keygen */
-//! # 0;
-//! let n = /* number of signers */
-//! # 3;
-//!
-//! let aux_info = cggmp21::aux_info_gen(eid, i, n, pregenerated_primes)
-//!     .start(&mut OsRng, party)
-//!     .await?;
-//! # Ok(()) }
-//! ```
-//!
-//! After keygen and aux info gen are done, you can make a “complete” key share that can be used for signing:
-//! ```rust,no_run
-//! # fn main() -> Result<(), cggmp21::key_share::InvalidKeyShare> {
 //! # let (incomplete_key_share, aux_info): (cggmp21::IncompleteKeyShare<cggmp21::supported_curves::Secp256k1>, cggmp21::key_share::AuxInfo) = unimplemented!();
 //! let key_share = cggmp21::KeyShare::from_parts((incomplete_key_share, aux_info))?;
-//! # Ok(()) }
+//! # Ok::<_, cggmp21::key_share::InvalidKeyShare>(())
 //! ```
 //!
 //! ### Signing
-//! Once a complete key share is obtained, signers can sign or generate presignatures. In either case, the required threshold t of
-//! signers must take part in the protocol. Each signer needs to be assigned a unique index
-//! `0 <= i < t`, but we also need to know which index each signer had at keygen.
+//! Once signers have a set of "completed" key shares, they can sign or generate presignatures.
+//! In either case, exactly the threshold number (i.e., t) of signers must take part in the protocol.
+//! As in the DKG protocol, each signer needs to be assigned a unique index, now in the range from 0
+//! to t-1. But the signers also need to know which index each signer occupied at the time of keygen.
 //!
 //! In the example below, we do a full signing:
 //! ```rust,no_run
@@ -181,11 +213,11 @@
 //! 1. Use [`SigningBuilder::generate_presignature`] to run the presignature generation protocol
 //! 2. Later, when a signing request is received, each signer issues a partial signature using
 //!    [`Presignature::issue_partial_signature`]
-//! 3. The requisite number of partial signatures can be combined using [`PartialSignature::combine`] to
+//! 3. A threshold number of partial signatures can be combined using [`PartialSignature::combine`] to
 //!    obtain a full signature
 //!
 //! **Never reuse presignatures!** If you use the same presignature to sign two different messages,
-//! it leaks information about key shares to anyone who can observe the signatures.
+//! the private key may be leaked.
 //!
 //! ## HD wallets support
 //! Library supports non-hardened deterministic key derivation based on [slip10] standard (compatible
