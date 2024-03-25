da1b97d483010479cdd21e6a021e1924cd73e944
Denis Varlakov <denis@dfns.co>
2024-03-25 16:51:10 +0100

    Reduce size of serialized share

--- key-share/src/lib.rs
+++ key-share/src/lib.rs
@@ -215,12 +215,17 @@ pub struct DirtyKeyInfo<E: Curve> {
     )]
     pub public_shares: Vec<NonZero<Point<E>>>,
     /// Verifiable secret sharing setup, present if key was generated using VSS scheme
+    #[cfg_attr(
+        feature = "serde",
+        serde(default, skip_serializing_if = "Option::is_none")
+    )]
     pub vss_setup: Option<VssSetup<E>>,
     /// Chain code associated with the key, if HD wallets support was enabled
     #[cfg(feature = "hd-wallets")]
     #[cfg_attr(
         feature = "serde",
         serde(default),
+        serde(skip_serializing_if = "Option::is_none"),
         serde(with = "As::<Option<utils::HexOrBin>>")
     )]
     #[cfg_attr(feature = "udigest", udigest(with = utils::encoding::maybe_bytes))]
@@ -240,6 +245,10 @@ pub struct VssSetup<E: Curve> {
     /// Key shares indexes
     ///
     /// `I[i]` corresponds to key share index of a $\ith$ signer
+    #[cfg_attr(
+        feature = "serde",
+        serde(with = "As::<Vec<generic_ec::serde::PreferCompact>>")
+    )]
     pub I: Vec<NonZero<Scalar<E>>>,
 }
 
