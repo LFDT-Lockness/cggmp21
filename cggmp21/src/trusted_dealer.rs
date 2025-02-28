13358bae6457f5154ff1171a7c678fc0ac5cc3f5
maurges <nikita@dfns.co>
2025-02-28 15:32:46 +0100

    Add pointful methods to cggmp trusted dealer too
    Signed-off-by: maurges <nikita@dfns.co>

--- cggmp21/src/trusted_dealer.rs
+++ cggmp21/src/trusted_dealer.rs
@@ -161,10 +161,31 @@ impl<E: Curve, L: SecurityLevel> TrustedDealerBuilder<E, L> {
 
     /// Generates [`KeyShare`]s
     ///
+    /// For Shamir secret sharing, it's shared at points `1` to `n`
+    ///
     /// Returns error if provided inputs are invalid, or if internal
     /// error has occurred.
     pub fn generate_shares(
+        self,
+        rng: &mut (impl RngCore + CryptoRng),
+    ) -> Result<Vec<KeyShare<E, L>>, TrustedDealerError> {
+        let preimages = (1..=self.n)
+            .map(|i| generic_ec::NonZero::from_scalar(generic_ec::Scalar::from(i)))
+            .collect::<Option<Vec<_>>>()
+            .ok_or(Reason::DeriveKeyShareIndex)?;
+        self.generate_shares_at(preimages, rng)
+    }
+
+    /// Generates [`KeyShare`]s shared at preimages provided. Each share is
+    /// going to have the given `preimages` as its `I` component.
+    ///
+    /// Preimages are ignored for additive key shares.
+    ///
+    /// Returns error if provided inputs are invalid, or if internal
+    /// error has occurred.
+    pub fn generate_shares_at(
         mut self,
+        preimages: Vec<NonZero<generic_ec::Scalar<E>>>,
         rng: &mut (impl RngCore + CryptoRng),
     ) -> Result<Vec<KeyShare<E, L>>, TrustedDealerError> {
         let n = self.n;
@@ -172,7 +193,7 @@ impl<E: Curve, L: SecurityLevel> TrustedDealerBuilder<E, L> {
         let enable_crt = self.enable_crt;
 
         let primes = self.pregenerated_primes.take();
-        let core_key_shares = self.inner.generate_shares(rng).map_err(Reason::CoreError)?;
+        let core_key_shares = self.inner.generate_shares_at(preimages, rng).map_err(Reason::CoreError)?;
         let aux_data = if let Some(primes) = primes {
             generate_aux_data_with_primes(rng, primes, enable_multiexp, enable_crt)?
         } else {
@@ -187,6 +208,30 @@ impl<E: Curve, L: SecurityLevel> TrustedDealerBuilder<E, L> {
             .map_err(|err| Reason::InvalidKeyShare(err.into_error()))?;
 
         Ok(key_shares)
+    }
+
+    /// Generates [`CoreKeyShare`]s shared at random points
+    ///
+    /// Returns error if provided inputs are invalid, or if internal
+    /// error has occurred.
+    ///
+    /// For Shamir secret sharing, the points at which the value is shared at
+    /// are chosen at random between `1` and `u16::MAX`. For additive shares,
+    /// this is the same as [`TrustedDealerBuilder::generate_shares`]
+    ///
+    /// Returns error if provided inputs are invalid, or if internal
+    /// error has occurred.
+    pub fn generate_shares_at_random(
+        self,
+        rng: &mut (impl rand_core::RngCore + rand_core::CryptoRng),
+    ) -> Result<Vec<KeyShare<E, L>>, TrustedDealerError> {
+        let key_shares_indexes =
+            rand::seq::index::sample(rng, usize::from(u16::MAX - 1), usize::from(self.n))
+                .iter()
+                .map(|i| generic_ec::NonZero::from_scalar(generic_ec::Scalar::from(i + 1)))
+                .collect::<Option<Vec<_>>>()
+                .ok_or(Reason::DeriveKeyShareIndex)?;
+        self.generate_shares_at(key_shares_indexes, rng)
     }
 }
 
@@ -289,6 +334,8 @@ enum Reason {
     BuildCrt(#[source] InvalidKeyShare),
     #[error("couldn't build multiexp tables")]
     BuildMultiexp(#[source] InvalidKeyShare),
+    #[error("deriving key share index failed")]
+    DeriveKeyShareIndex,
     #[error(transparent)]
     CoreError(#[from] key_share::trusted_dealer::TrustedDealerError),
 }
